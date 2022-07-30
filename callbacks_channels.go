c45def654ede2faf56e9368b5c927d4bd4f35917
Sapphire <quinton.oglesby@icloud.com>
2022-07-30 16:58:17 -0500

    Update callbacks_channels.go

--- callbacks_channels.go
+++ callbacks_channels.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -9,27 +10,13 @@ import (
 // This function will be called every time a new channel is made.
 func channelCreate(session *discordgo.Session, channel *discordgo.ChannelCreate) {
 
-	// Details of the newly created channel.
-	fmt.Printf("----------\nCreated Channel Details:\nChannel ID: %s\nGuild ID: %s\nChannel Name: %s\n",
-		channel.ID,
-		channel.GuildID,
-		channel.Name)
-	switch channel.Type {
-	case 0:
-		fmt.Printf("Channel Topic: %s\nChannel Type: Text\n", channel.Topic)
-
-		// Want to create a new web hook in the text channel that was just created.
-		webhook, err := session.WebhookCreate(channel.ID, channel.Name, "")
+	// Create a mimicry webhook in the newly created channel.
+	if channel.Type == 0 {
+		webhook, err := session.WebhookCreate(channel.ID, fmt.Sprintf("%s-mimic", channel.Name), "")
 		if err != nil {
 			fmt.Println("Webhook Creation Error ", err)
 		}
-		fmt.Printf("Webhook successfully created with the name '%s'\n", webhook.Name)
-	case 2:
-		fmt.Println("Channel Type: Voice")
-	case 4:
-		fmt.Println("Channel Type: Category")
-	default:
-		fmt.Println("Channel Type: Other")
+		log.Printf("Webhook successfully created with the name '%s'\n", webhook.Name)
 	}
 
 }
