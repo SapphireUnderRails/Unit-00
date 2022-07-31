c75d6e7f8185d240088150add4accc6364b529e1
Sapphire <quinton.oglesby@icloud.com>
2022-07-30 19:23:12 -0500

    Update database.go

--- database.go
+++ database.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
+	"os"
 
 	"github.com/go-sql-driver/mysql"
 )
 
+type Config struct {
+	Username string
+	Password string
+	Database string
+}
+
 func message_database() sql.DB {
+
+	// Retrieve the configuration information from sql_data.json file.
+	fileContent, err := os.ReadFile("sql_data.json")
+	if err != nil {
+		log.Println("Could not open sql_data file.")
+		log.Fatal(err)
+	}
+
+	// Unmarshal the token from the file contnet to grab the token.
+	var config Config
+	json.Unmarshal(fileContent, &config)
 
 	// Set up the configuration for the database connection.
 	configuration := mysql.Config{
-		User:   "root",
-		Passwd: "KingArthur09052012?",
+		User:   config.Username,
+		Passwd: config.Password,
 		Net:    "tcp",
 		Addr:   "127.0.0.1:3306",
-		DBName: "discord_messages",
+		DBName: config.Database,
 	}
 
 	// Open a connection to the discord_messages database.
